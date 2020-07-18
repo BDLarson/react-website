3df855cde9b51c0fde505f9d6e4211246bf38837
Blake Larson <larsonbd1@gmail.com>
2020-07-18 01:42:16 -0700

    Update links and website logo.
    Added Github, LinkedIn and website logo.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/self_image.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="website logo" />
         <p>
-          Hi, my name is Blake. Welcome to this page. Starting initially changes...
+          Hi, my name is Blake. Welcome to this page. Starting initial changes...
         </p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <a
+          className="App-link"
+          href="https://github.com/blake417"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/blake-d-larson/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
         <a
           className="App-link"
           href="https://reactjs.org"
